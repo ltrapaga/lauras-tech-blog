97dbf82bb87bd2e0d78510137d78f8f5711c90a0
Laura Trapaga <trapaga9@gmail.com>
2023-02-18 18:43:26 -0500

    Edit maxAge and add comments to server.js

--- server.js
+++ server.js
@@ -1,46 +1,53 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// Dependencies
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+// Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
+// Custom Handlebars helpers
 const hbs = exphbs.create({ helpers });
 
+// Session with cookies
 const sess = {
-  secret: 'secret',
+  secret: process.env.SECRET,
   cookie: {
-    maxAge: 300000,
+    maxAge: 5 * 60 * 1000, // Expires after 5 minutes
     httpOnly: true,
     secure: false,
-    sameSite: 'strict',
+    sameSite: "strict",
   },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+// Turn on routes
 app.use(routes);
 
+// Set default template engine to Handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Connect to the database before starting the Express.js server
+// Force false to prevent data drop on every sync
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  // Asynchronized callback
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
 });
