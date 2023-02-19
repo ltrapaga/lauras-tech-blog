1ed154ac643370cb41920877bf44bde7b713c81a
Laura Trapaga <trapaga9@gmail.com>
2023-02-18 19:58:24 -0500

    Edit and add comments to auth.js and helpers.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,11 @@
-// Authentication middleware
+// Auth middleware
 const withAuth = (req, res, next) => {
-    if (!req.session.logged_in) {
-      res.redirect('/login');
-    } else {
-      // If your logged in I call the next route
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,11 @@
 module.exports = {
-    // take something that looks like 001930:00 GMT to 7:30PM
-    format_time: (date) => {
-      return date.toLocaleTimeString();
-    },
-    format_date: (date) => {
-        // returning new object for DATE, passing in the date value ex(2/2/2023) breaking it down to look like <=
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-        new Date(date).getFullYear() 
-      }`;
-    },
-  };
+  // Take in a timestamp
+  format_time: (date) => {
+    // Return a string with only the time
+    return date.toLocaleTimeString();
+  },
+  format_date: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+  },
+};
